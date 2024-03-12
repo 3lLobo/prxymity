cda61f6d2e30fca572682d97fdb1091cb454b41e
3lLobo <flocwolf@gmail.com>
2024-03-12 15:04:25 -0400

    Level up

--- mitm_proxy/run_addons.py
+++
@@ -1,7 +0,0 @@
-from my_addons.dump_body import DumpBody
-from my_addons.add_modelid import AddModelId
-
-MODEL_NAME = "gpt-4-0125-preview"
-MODEL_NAME = "TheBloke/Llama-2-13B-chat-AWQ"
-
-addons = [DumpBody(), AddModelId(MODEL_NAME)]
