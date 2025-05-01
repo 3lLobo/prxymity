552ed7de2772a2a5d32590f0557002135da5fc4c
lumi <lumi@localhost.localdomain>
2025-05-01 14:39:45 +0200

    env renames

---
+++ startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#  start minikube
+# minikube start
+
+# Load .env file
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+# Load python venv
+if [ -d .venv ]; then
+    source .venv/bin/activate
+else
+    echo "Python virtual environment not found. Please create it first."
+    exit 1
+fi
+
+# start the proxy in background
+mitmdump -s run_addons.py -m reverse:$LLM_HOST:$RP_PORT & 
+
+# run license script
+python es_license.py --es_url $ES_HOST --username $ES_USER --password $ES_PASSWORD --insecure --license_file $LICENSE_FILE
