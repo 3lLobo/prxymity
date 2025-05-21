35116003ade046bcfa12bd8bbe635353fc63a1ac
3lLobo <flocwolf@gmail.com>
2025-05-21 14:20:02 +0200

    debugging the alert formatting

--- convert_alerts.py
+++ convert_alerts.py
@@ -8,50 +8,49 @@ class AlertFormatter:
     a flattened string representation.
     """
 
-    def __init__(self, alert: Dict[str, Any]) -> None:
+    def __init__(
+        self,
+        alert: Dict[str, Any],
+        fields: List[str] = None,
+    ) -> None:
         """
         Initialize the formatter with an alert dictionary.
 
         :param alert: A dictionary representing a single alert.
+        :param fields: A list of fields to include in the output.
         """
         self.alert = alert
-        self.fields: List[str] = [
-            "@timestamp",
-            "_id",
-            "kibana.alert.original_time",
-            "kibana.alert.risk_score",
-            "kibana.alert.rule.description",
-            "kibana.alert.rule.name",
-            "kibana.alert.rule.threat.framework",
-            "kibana.alert.rule.threat.tactic.id",
-            "kibana.alert.rule.threat.tactic.name",
-            "kibana.alert.rule.threat.tactic.reference",
-            "kibana.alert.rule.threat.technique.id",
-            "kibana.alert.rule.threat.technique.name",
-            "kibana.alert.rule.threat.technique.reference",
-            "kibana.alert.severity",
-            "kibana.alert.workflow_status",
-            "process.executable",
-        ]
+        self.fields = fields or []
 
     def get_nested(self, d: Dict[str, Any], key_path: str) -> Union[str, int]:
         """
         Safely retrieve a nested value from a dictionary using
         a dotted key path.
 
-        :param d: The dictionary to retrieve the value from.
+        :param d: The dictionary to search.
         :param key_path: A dot-separated string indicating
         the path to the value.
         :return: The value if found and of type str or int,
         otherwise an empty string.
         """
         keys = key_path.split(".")
+        # deep copy the dictionary to avoid modifying the original
         for key in keys:
+            if isinstance(d, list):
+                d = d[0] if d else {}
+
             if isinstance(d, dict):
                 d = d.get(key, {})
             else:
                 return ""
-        return d if isinstance(d, (str, int)) else ""
+        if isinstance(d, (str, int)):
+            return d
+        if isinstance(d, float):
+            return int(d)
+        elif isinstance(d, list):
+            return ", ".join(str(item) for item in d)
+
+        return ""
 
     def format(self) -> str:
         """
@@ -60,9 +59,30 @@ class AlertFormatter:
         :return: A formatted string representing the alert.
         """
         output = []
+        field_threat = "kibana.alert.rule.threat"
+
         for field in self.fields:
-            value = self.get_nested(self.alert, field)
-            output.append(f"{field},{value}")
+            if field in self.alert.keys():
+                value = self.alert.get(field, "")
+
+            elif field.startswith(field_threat):
+                # Handle nested fields under "kibana.alert.rule.threat"
+                nested_field = field[len(field_threat) + 1 :]
+                nested_alert = self.alert.copy().get(field_threat, {})
+                value = self.get_nested(nested_alert, nested_field)
+
+            else:
+                value = self.get_nested(self.alert.copy(), field)
+
+            if value:
+                if field.startswith("kibana.alert"):
+                    # Remove the prefix "kibana.alert."
+                    field = field[len("kibana.alert.") :]
+
+                output.append(f"{field},{value}")
+            # else:
+            #     output.append(f"Field '{field}' not found in alert data.")
+
         return "\n".join(output)
 
 
@@ -71,16 +91,27 @@ class AlertProcessor:
     A class to process a JSON file of alerts and format each alert.
     """
 
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str, key_path: str) -> None:
         """
         Initialize the processor with the path to the alerts JSON file.
 
         :param file_path: Path to the alerts JSON file.
         """
         self.file_path = file_path
-        self.data: Dict[str, Any] = self.load_data()
+        self.key_path = key_path
+        self.data: Dict[str, Any] = self._load_data()
+        self.keys: List[str] = self._load_keys()
 
-    def load_data(self) -> Dict[str, Any]:
+    def _load_keys(self) -> List[str]:
+        """
+        Load the keys from a list file.
+
+        :return: A list of keys to be used for formatting.
+        """
+        with open(self.key_path, "r", encoding="utf-8") as f:
+            return [line.strip() for line in f]
+
+    def _load_data(self) -> Dict[str, Any]:
         """
         Load and parse the JSON data from the file.
 
@@ -93,15 +124,15 @@ class AlertProcessor:
         """
         Process and format each alert in the JSON data.
         """
-        hits = self.data.get("hits", [])
+        hits = self.data
         for hit in hits:
             alert = hit.get("_source", {})
             alert["_id"] = hit.get("_id", "")
-            formatter = AlertFormatter(alert)
+            formatter = AlertFormatter(alert, fields=self.keys)
             print(formatter.format())
             print()  # Spacing between alerts
 
 
 if __name__ == "__main__":
-    processor = AlertProcessor("data/alerts.json")
+    processor = AlertProcessor("data/alerts.json", "data/keys.txt")
     processor.process_alerts()
